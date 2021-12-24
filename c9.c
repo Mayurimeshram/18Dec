c467df93d30cd0ae33e83328d1013438156aaf52
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-24 17:42:12 +0530

    Add files via upload

---
+++ c9.c
@@ -0,0 +1,27 @@
+#include<stdio.h>;
+int main()
+{
+	char ch;
+	printf(" enter any alphabets");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+	
+	case'a':
+	
+	case'A':
+		
+	case'e':
+		case'E':
+		case'i':
+			case'I':
+			case'o':
+				case'O':
+				  case'u':
+				  case'U':
+				  printf(" your alphabets is vowel\n");
+				  break;
+				  default:
+				  printf(" your alphabets  is consonent\n");
+}
+}
