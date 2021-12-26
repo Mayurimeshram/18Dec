c97c94707a08f8f79bcfbd194a80dcd7dfcc9a11
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-26 22:09:16 +0530

    Add files via upload

---
+++ c14.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	 int n,i,k=0;
+	 printf(" enter number");
+	 scanf(" %d",&n);
+	 i=1;
+	 while(i<=0)
+	 {
+	 	if(n%i==0);
+	// printf(" no is not prime");
+	 	{
+	 		k=k+1;
+		 }
+ i++;
+	 } 
+	 if(k==2)
+	// printf(" no is prime");
+	 {
+	 	printf(" no is prime");
+	 } 
+	 
+	 else{
+	 	printf(" no is not prime");
+	 }
+}  
+
+
+
+
+
